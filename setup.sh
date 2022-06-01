68db4c80f9a24445f460902c827a27182737812f
Henrique Navarro <ricksnavarro@gmail.com>
2022-06-01 18:06:55 -0300

    Add column is_authorized to table log

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 sqlite3 access-control.db "create table access (RFID integer, weekday text, start_time text, end_time text, primary key (RFID, weekday))"
-sqlite3 access-control.db "create table log (RFID integer, date_time text, primary key (RFID, date_time))"
+sqlite3 access-control.db "create table log (RFID integer, date_time text, is_authorized integer, primary key (RFID, date_time))"
